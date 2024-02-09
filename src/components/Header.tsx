44ac7558741bec65f39dc5cbc23d15d6f236ab4f
Marvin <marvin.kramer.lol@gmail.com>
2024-02-09 13:53:40 +0100

    added the username to the header to show who is logged in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,13 @@ import {createClient} from "@/utils/supabase/server";
 import {cookies} from "next/headers";
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
+import {Profile} from "@/types/profile";
 
 const Header = async () => {
-    const session = createClient(cookies())
-    const {data} = await session.auth.getUser()
+    const supabase = createClient(cookies())
+    const {data: userData} = await supabase.auth.getUser()
+    const {data: profileData} = await supabase.from('profile').select().eq("user_id", userData.user?.id)
+    const profile = profileData?.at(0) as Profile | null
 
     return (
         <div className={"p-3 flex border-b"}>
@@ -15,7 +18,13 @@ const Header = async () => {
                 <Link href={"/"} className={"text-2xl"}>DiaryLens</Link>
             </div>
             <div className={"flex-1 flex justify-end items-center"}>
-                {data.user ? <p>{data.user.email}</p> : <Link href={'/login'}><Button size={'sm'}>Login</Button></Link>}
+                {profile ? (
+                    <p>{profile.first_name}</p>
+                ) : (
+                    <Link href={'/login'}>
+                        <Button size={'sm'}>Login</Button>
+                    </Link>
+                )}
             </div>
         </div>
     );
---
+++ src/types/profile.tsx
@@ -0,0 +1,5 @@
+export interface Profile {
+    id: string,
+    first_name: string,
+    last_name: string,
+}
