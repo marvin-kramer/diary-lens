968f365e1f4cb411a9f1778489357f7ae07b2daf
Marvin <marvin.kramer.lol@gmail.com>
2024-02-11 11:09:46 +0100

    optimized a few things in the tag-select and added option to delete a tag

--- src/components/ui/tag-select.tsx
+++ src/components/ui/tag-select.tsx
@@ -8,9 +8,16 @@ import {
     CommandDialog,
     CommandGroup,
     CommandItem,
-    CommandList
+    CommandList,
 } from "@/components/ui/command";
 import {createClient} from "@/utils/supabase/client";
+import {PlusCircle, Trash2} from "lucide-react";
+import {
+    AlertDialog, AlertDialogAction, AlertDialogCancel,
+    AlertDialogContent, AlertDialogDescription, AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 const TagSelect = ({initialSelectedTag, diary_id}: { initialSelectedTag: Tag[], diary_id: string }) => {
     const supabase = createClient()
@@ -33,12 +40,13 @@ const TagSelect = ({initialSelectedTag, diary_id}: { initialSelectedTag: Tag[], 
         const {data, error} = await supabase.from("diary_tag").select(`
             diary_id,
             tag ( id, created_at, user_id, name )
-        `).eq("diary_id", diary_id)
+        `).eq("diary_id", diary_id).order("created_at")
         const tags = data?.map(tag => tag.tag as unknown as Tag)
         if (tags) setCurrentlySelectedTags(tags)
     }
 
     async function addTagToDiary(tag: Tag) {
+        console.log("on add tag:", tag)
         setCurrentInputValue("")
         const {error} = await supabase.from("diary_tag").upsert({diary_id: diary_id, tag_id: tag.id})
         if (error) console.log(error)
@@ -60,6 +68,12 @@ const TagSelect = ({initialSelectedTag, diary_id}: { initialSelectedTag: Tag[], 
         }
     }
 
+    async function deleteTag(tag: Tag) {
+        const {error} = await supabase.from("tag").delete().eq("id", tag.id)
+        setUserTagData()
+        setSelectedTagData()
+    }
+
 
     return (
         <>
@@ -67,13 +81,13 @@ const TagSelect = ({initialSelectedTag, diary_id}: { initialSelectedTag: Tag[], 
                 className={cn("flex flex-wrap gap-2 p-2 rounded-md hover:bg-secondary/60 hover:cursor-pointer w-full")}
                 onClick={() => setInputActive(true)}
             >
-                {currentlySelectedTags.map(tag => (
+                {currentlySelectedTags.length > 0 ? currentlySelectedTags.map(tag => (
                     <Badge key={tag.id}>{tag.name}</Badge>
-                ))}
+                )) : <p className={"text-muted-foreground"}>Add tags to your diary...</p>}
             </div>
 
             <CommandDialog open={inputActive} onOpenChange={setInputActive}>
-                <div className={"flex flex-wrap gap-2 items-center border-b p-3"}>
+                <div className={"flex flex-wrap gap-2 items-center border-b p-3 pr-11"}>
                     {currentlySelectedTags.map(tag => (
                         <Badge key={tag.id} className={"w-fit"}>{tag.name}</Badge>
                     ))}
@@ -81,7 +95,7 @@ const TagSelect = ({initialSelectedTag, diary_id}: { initialSelectedTag: Tag[], 
                         onKeyDown={(event) => checkForBackKey(event)}
                         onValueChange={setCurrentInputValue}
                         value={currentInputValue}
-                        className={"!h-fit flex-1 bg-transparent text-sm outline-none placeholder:text-muted-foreground"}
+                        className={"!h-fit min-w-4 flex-1 bg-transparent text-sm outline-none placeholder:text-muted-foreground"}
                     />
                 </div>
                 <CommandList onSelect={(event) => console.log(event)}>
@@ -90,13 +104,18 @@ const TagSelect = ({initialSelectedTag, diary_id}: { initialSelectedTag: Tag[], 
                             <CommandItem
                                 key={tag.id}
                                 onSelect={() => addTagToDiary(tag)}
-                            >{tag.name}</CommandItem>
+                                className={"group"}
+                            >
+                                <span className={"w-full overflow-hidden text-ellipsis text-nowrap"}>{tag.name}</span>
+                                <DeleteDialog tag={tag} onDelete={deleteTag}/>
+                            </CommandItem>
                         ))}
                     </CommandGroup>
-                    {currentInputValue.length > 0 &&
+                    {currentInputValue.trim().length > 0 &&
                         <CommandGroup heading="Neu">
-                            <CommandItem className={"flex gap-2"} onSelect={() => createNewTag(currentInputValue)}>
-                                {currentInputValue}
+                            <CommandItem className={"flex gap-2"} onSelect={() => createNewTag(currentInputValue.trim())} value={currentInputValue.trim()}>
+                                <PlusCircle/>
+                                <span className={"w-full overflow-hidden text-nowrap text-ellipsis"}>{currentInputValue.trim()}</span>
                             </CommandItem>
                         </CommandGroup>
                     }
@@ -105,6 +124,41 @@ const TagSelect = ({initialSelectedTag, diary_id}: { initialSelectedTag: Tag[], 
         </>
     );
 };
+
+
+const DeleteDialog = ({onDelete, tag}: { tag: Tag, onDelete: (tag: Tag) => void }) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <>
+            <Trash2
+                className={"hidden group-aria-[selected]:block hover:cursor-pointer"}
+                onClick={(event) => {
+                    event.stopPropagation()
+                    setIsOpen(true)
+                }}
+            />
+            <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete this tag
+                            and remove it from every diary entry.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction onClick={(event) => {
+                            event.stopPropagation()
+                            onDelete(tag)
+                        }}>Delete</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+        </>
+    )
+}
 
 function contains(tags: Tag[], name: string): boolean {
     const foundTag = tags.find(value => value.name === name)
